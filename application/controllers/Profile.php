bc8a0ab38848bdf11fe29b6b4fe41177eabbfd5d
Rafli Firdausy Irawan <rafly.firdausy@gmail.com>
2019-12-28 02:36:11 +0700

    done edit profile

--- application/controllers/Profile.php
+++ application/controllers/Profile.php
@@ -1,13 +1,75 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Profile extends MY_Controller {
-    public function ___construct(){
-        parent::___construct();
+class Profile extends MY_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
     }
 
-    public function index(){
-        return $this->loadView('profile.show-profile');
+    public function index()
+    {
+        $dataUser   = $this->m_data->getWhere("id_user", $this->session->userdata("login")->id_user);
+        $dataUser   = $this->m_data->getData("user")->row();
+        
+        $data["user"]   = $dataUser;
+        return $this->loadView('profile.show-profile', $data);
     }
 
+    public function update_biodata(){
+        $nama       = $this->input->post("nama");
+        $username   = $this->input->post("username");
+        $no_hp      = $this->input->post("no_hp");
+        $alamat     = $this->input->post("alamat");
+
+        $data       = [
+            "nama_lengkap"  => $nama,
+            "username"       => $username,
+            "nomor_hp"      => $no_hp,
+            "alamat"        => $alamat
+        ];
+        if(strlen($username >= 5)){
+            $updateData = $this->m_data->update("user", $data, ["id_user" => $this->session->userdata("login")->id_user]);
+            if ($updateData == 1) {
+                $this->session->set_flashdata("sukses", "Mengedit data $nama pada database");
+            } else {
+                $this->session->set_flashdata("gagal", $this->m_data->getError());
+            }
+        } else {
+            $this->session->set_flashdata("gagal", "Username Minimal 5 Karakter!");
+        }
+        
+        redirect(base_url("profile"));
+    }
+
+    public function update_password(){
+        $password_sekarang          = $this->input->post("password_sekarang");
+        $password_baru              = $this->input->post("password_baru");
+        $konfirmasi_password_baru   = $this->input->post("konfirmasi_password_baru");
+
+        $cekPass    = $this->m_data->getWhere("id_user", $this->session->userdata("login")->id_user);
+        $cekPass    = $this->m_data->getWhere("password", md5($password_sekarang));
+        $cekPass    = $this->m_data->getData("user")->row();
+
+        if($cekPass){
+            if(strlen($password_baru) >= 8){
+                if($password_baru == $konfirmasi_password_baru){
+                    $updateData = $this->m_data->update("user", ["password" => md5($password_baru)], ["id_user" => $this->session->userdata("login")->id_user]);
+                    if ($updateData == 1) {
+                        $this->session->set_flashdata("sukses", "Ubah password pada database");
+                    } else {
+                        $this->session->set_flashdata("gagal", $this->m_data->getError());
+                    }      
+                } else {                
+                    $this->session->set_flashdata("gagal", "Konfirmasi Password Baru Salah!");
+                }
+            } else {
+                $this->session->set_flashdata("gagal", "Password minimal 8 Karakter!");
+            }            
+        } else {
+            $this->session->set_flashdata("gagal", "Password lama yang anda masukan salah!");
+        }
+        redirect(base_url("profile"));
+    }
 }
--- application/views/profile/show-profile.blade.php
+++ application/views/profile/show-profile.blade.php
@@ -19,7 +19,7 @@ User Profile
 
 @section('page-content')
     <div class="row">
-        <div class="col-md-4">
+        {{-- <div class="col-md-4">
             <div class="box box-success">
                 <div class="box-body box-profile">
                     <img class="profile-user-img img-responsive img-circle" src="{{ asset('dist/img/user4-128x128.jpg') }}" alt="User profile picture">
@@ -38,8 +38,23 @@ User Profile
                 </ul>
                 </div>        
             </div>
-        </div>
-        <div class="col-md-8">
+        </div> --}}
+        <div class="col-md-12">
+            @if ($CI->session->flashdata("sukses"))
+            <div class="alert alert-success alert-dismissible">
+                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                <h4><i class="icon fa fa-check"></i> Sukses</h4>
+                {{ $CI->session->flashdata("sukses") }}
+            </div>
+            @endif      
+            
+            @if ($CI->session->flashdata("gagal"))
+            <div class="alert alert-danger alert-dismissible">
+                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                <h4><i class="icon fa fa-check"></i> Gagal</h4>
+                {{ $CI->session->flashdata("gagal") }}
+            </div>
+            @endif   
             <div class="nav-tabs-custom">
                 <ul class="nav nav-tabs">
                     <li class="active"><a href="#biodata-tab" data-toggle="tab">Biodata</a></li>
@@ -48,32 +63,33 @@ User Profile
                 <div class="tab-content">
 
                     <div class="tab-pane active" id="biodata-tab">
-                        <form action="#" class="form-horizontal">
+                        <form method="POST" action="{{ base_url('profile/update-biodata') }}" class="form-horizontal">
                             <div class="form-group">
                                 <label for="nama" class="col-sm-2 control-label">Nama</label>
                                 <div class="col-sm-10">
-                                    <input type="text" name="nama" class="form-control" id="nama" placeholder="Masukan nama lengkap">
+                                    <input required type="text" value="{{ $user->nama_lengkap }}" name="nama" class="form-control" id="nama" placeholder="Masukan nama lengkap">
                                 </div>
                             </div>
                             <div class="form-group">
-                                <label for="email" class="col-sm-2 control-label">Email</label>
+                                <label for="username" class="col-sm-2 control-label">Username</label>
                                 <div class="col-sm-10">
-                                    <input type="email" name="email" class="form-control" id="email" placeholder="Masukan Email">
+                                    <input required type="text" value="{{ $user->username }}" name="username" class="form-control" id="usernameCoy" placeholder="Masukan Username">
+                                    <small id="errorUsername" style="color: red"></small>
                                 </div>
                             </div>
                             <div class="form-group">
                                 <label for="no_hp" class="col-sm-2 control-label">Nomor Hp</label>
                                 <div class="col-sm-10">
-                                    <input type="tel" class="form-control" id="no_hp" placeholder="Masukan nomor handphone">
+                                    <input name="no_hp" required type="tel" pattern="[0-9]+" value="{{ $user->nomor_hp }}" class="form-control" id="no_hp" placeholder="Masukan nomor handphone">
                                 </div>
                             </div>
                             <div class="form-group">
                                 <label for="alamat" class="col-sm-2 control-label">Alamat</label>
                                 <div class="col-sm-10">
-                                    <textarea class="form-control" name="alamat" id="alamat" placeholder="Masukan Alamat Lengkap"></textarea>
+                                    <textarea required class="form-control" name="alamat" id="alamat" placeholder="Masukan Alamat Lengkap">{{ $user->alamat }}</textarea>
                                 </div>
                             </div>
-                            <div class="form-group">
+                            {{-- <div class="form-group">
                                 <label for="inputSkills" class="col-sm-2 control-label">Jenis Kelamin</label>            
                                 <div class="col-sm-10">
                                     <div class="radio">
@@ -92,7 +108,7 @@ User Profile
                                     <input class="form-control pull-right" type="file" name="foto" id="foto">
                                     <p class="help-block">Tipe File */image , Max size : 5 Mb</p>
                                 </div>
-                            </div>
+                            </div> --}}
                             <div class="form-group">
                                 <div class="col-sm-offset-2 col-sm-10">
                                     <button type="submit" class="btn btn-primary">Simpan</button>
@@ -102,23 +118,23 @@ User Profile
                     </div>
 
                     <div class="tab-pane" id="password-tab">
-                        <form action="#" class="form-horizontal">
+                        <form method="POST" action="{{ base_url('profile/update-password') }}" class="form-horizontal">
                             <div class="form-group">
                                 <label for="password_sekarang" class="col-sm-3 control-label">Password Lama</label>
                                 <div class="col-sm-9">
-                                    <input type="password" name="password_sekarang" class="form-control" id="nama" placeholder="Masukan password sekarang">
+                                    <input type="password" name="password_sekarang" class="form-control" placeholder="Masukan password sekarang">
                                 </div>
                             </div>
                             <div class="form-group">
                                 <label for="password_baru" class="col-sm-3 control-label">Password Baru</label>
                                 <div class="col-sm-9">
-                                    <input type="password" name="password_baru" class="form-control" id="email" placeholder="Masukan password baru">
+                                    <input type="password" name="password_baru" class="form-control"  placeholder="Masukan password baru">
                                 </div>
                             </div>
                             <div class="form-group">
                                 <label for="konfirmasi_password_baru" class="col-sm-3 control-label">Konfirmasi Password Baru</label>
                                 <div class="col-sm-9">
-                                    <input type="password" class="form-control" id="konfirmasi_password_baru" placeholder="Konfirmasi password baru">
+                                    <input type="password" class="form-control" name="konfirmasi_password_baru" placeholder="Konfirmasi password baru">
                                 </div>
                             </div>
                             <div class="form-group">
