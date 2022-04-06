11d45b28b68634fe51bd27ee5d959b8810584a82
Andi Fahruddin Akas <andifahruddinakas@gmail.com>
2022-04-07 00:34:09 +0800

    Hapus package fzaninotto/faker

--- application/helpers/rafly_helper.php
+++ application/helpers/rafly_helper.php
@@ -1,10 +1,6 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-function faker(){
-    return Faker\Factory::create('id_ID');
-}
-
 function d($x){
     return die(json_encode($x));
 }
